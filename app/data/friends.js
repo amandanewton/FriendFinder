20f0aae71eb8cf350617cc2b392eb3f6e6fb1ee2
amandanewton <47038978+amandanewton@users.noreply.github.com>
2019-06-21 13:34:05 -0400

    BIP

---
+++ app/data/friends.js
@@ -0,0 +1,42 @@
+var friendsData = [
+    {
+      name: "Derek Peth",
+      photo: "https://akns-images.eonline.com/eol_images/Entire_Site/2019517/rs_1024x683-190617203624-1024x683.bachelor-in-paradise-derek-lp.61719.jpg?fit=inside|900:650&output-quality=90",
+      scores: [3, 4, 3, 3, 4, 4, 4, 1, 5, 3]
+    },
+    {
+      name: "Caelynn Miller-Keyes",
+      photo:"https://akns-images.eonline.com/eol_images/Entire_Site/2019517/rs_683x1024-190617203625-1024x683.bachelor-in-paradise-caelynn-lp.61719.jpg?fit=inside|900:650&output-quality=90",
+      scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    },
+    {
+      name: "Blake Horstmann",
+      photo:"https://akns-images.eonline.com/eol_images/Entire_Site/2019517/rs_634x1024-190617092303-634-9-bachelor-in-paradise-cast-ch-061719.jpg?fit=inside|900:650&output-quality=90",
+      scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+      name: "Demi Burnett",
+      photo:"https://akns-images.eonline.com/eol_images/Entire_Site/2019517/rs_634x1024-190617092300-634-10-bachelor-in-paradise-cast-ch-061719.jpg?fit=inside|900:650&output-quality=90",
+      scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+    {
+      name: "John Paul Jones",
+      photo:"https://akns-images.eonline.com/eol_images/Entire_Site/2019517/rs_634x1024-190617092301-634-7-bachelor-in-paradise-cast-ch-061719.jpg?fit=inside|900:650&output-quality=90",
+      scores: [5, 4, 4, 4, 4, 3, 2, 1, 5, 5]
+    },
+    {
+      name: "Tayshia Adams",
+      photo:"https://akns-images.eonline.com/eol_images/Entire_Site/2019517/rs_634x1024-190617092300-634-bachelor-in-paradise-cast-ch-061719.jpg?fit=inside|900:650&output-quality=90",
+      scores: [2, 4, 2, 4, 2, 3, 4, 4, 4, 3]
+    },
+    {
+      name: "Hannah Godwin",
+      photo:"https://akns-images.eonline.com/eol_images/Entire_Site/2019517/rs_683x1024-190617203629-1024x683.bachelor-in-paradise-hannah-g-lp.61719.jpg?fit=inside|900:650&output-quality=90",
+      scores: [5, 4, 5, 4, 5, 3, 3, 3, 2, 4]
+    }
+  ];
+  
+  
+  
+  // Export to API Route
+  module.exports = friendsData;
